32fc8b23a2c5e92520a7b337ee6d50c20be509d4
Dovguchic Evgeniy <dovgevgen@gmail.com>
2019-03-05 02:46:06 +0300

    added to json-server db new entities

--- DMC-UI/e2e/json-server/db.js
+++ DMC-UI/e2e/json-server/db.js
@@ -135,5 +135,193 @@ module.exports = {
                 }
             }
         }
+    ],
+    datasets: [
+        {
+            "metaDataId": 1,
+            "name": "telecom",
+            "description": "",
+            "attributes": {
+                "Total intl minutes": {
+                    "name": "Total intl minutes",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total day charge": {
+                    "name": "Total day charge",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total day calls": {
+                    "name": "Total day calls",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Number vmail messages": {
+                    "name": "Number vmail messages",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total night charge": {
+                    "name": "Total night charge",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Churn": {
+                    "name": "Churn",
+                    "type": "NOMINAL",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total night minutes": {
+                    "name": "Total night minutes",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total eve calls": {
+                    "name": "Total eve calls",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total day minutes": {
+                    "name": "Total day minutes",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Customer service calls": {
+                    "name": "Customer service calls",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total intl charge": {
+                    "name": "Total intl charge",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Area code": {
+                    "name": "Area code",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total night calls": {
+                    "name": "Total night calls",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "International plan": {
+                    "name": "International plan",
+                    "type": "NOMINAL",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "State": {
+                    "name": "State",
+                    "type": "NOMINAL",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Account length": {
+                    "name": "Account length",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total intl calls": {
+                    "name": "Total intl calls",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total eve minutes": {
+                    "name": "Total eve minutes",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Total eve charge": {
+                    "name": "Total eve charge",
+                    "type": "NUMERIC",
+                    "multiplier": 1.0,
+                    "checked": true
+                },
+                "Voice mail plan": {
+                    "name": "Voice mail plan",
+                    "type": "NOMINAL",
+                    "multiplier": 1.0,
+                    "checked": true
+                }
+            },
+            "dtoId": 1,
+            "_links": {
+                "self": {
+                    "href": "http://localhost:8081/rest/1/project/1/dataset/1"
+                },
+                "getDataset": {
+                    "href": "http://localhost:8081/rest/1/project/1/dataset/1"
+                },
+                "addDataset": {
+                    "href": "http://localhost:8081/rest/1/project/1/dataset"
+                },
+                "getDatasetList": {
+                    "href": "http://localhost:8081/rest/1/project/1/dataset/all"
+                },
+                "updateDataset": {
+                    "href": "http://localhost:8081/rest/1/project/1/dataset"
+                },
+                "deleteDataset": {
+                    "href": "http://localhost:8081/rest/1/project/1/dataset/1"
+                }
+            }
+        }
+    ],
+    algorithms: [
+        {
+            "algorithmId": 1,
+            "name": "Algorithm 1",
+            "description": null,
+            "project": null,
+            "dataSource": null,
+            "srcAttributes": null,
+            "dataDestination": null,
+            "method": null,
+            "dtoId": 1,
+            "_links": {
+                "self": {
+                    "href": "http://localhost:8081/rest/1/project/1/algorithm/1"
+                },
+                "addAlgorithm": {
+                    "href": "http://localhost:8081/rest/1/project/1/algorithm",
+                    "type": "POST"
+                },
+                "getAlgorithm": {
+                    "href": "http://localhost:8081/rest/1/project/1/algorithm/1",
+                    "type": "GET"
+                },
+                "getAlgorithmList": {
+                    "href": "http://localhost:8081/rest/1/project/1/algorithm/all",
+                    "type": "GET"
+                },
+                "updateAlgorithm": {
+                    "href": "http://localhost:8081/rest/1/project/1/algorithm",
+                    "type": "PUT"
+                },
+                "deleteAlgorithm": {
+                    "href": "http://localhost:8081/rest/1/project/1/algorithm/1",
+                    "type": "DELETE"
+                }
+            }
+        }
     ]
 }
