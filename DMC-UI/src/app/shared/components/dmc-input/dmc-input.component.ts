3cb9e04d28d745c6fdedb55409f64f8de20091b5
Dovguchic Evgeniy <dovgevgen@gmail.com>
2019-03-11 23:33:20 +0300

    updated the dmc input component

--- DMC-UI/src/app/shared/components/dmc-input/dmc-input.component.ts
+++ DMC-UI/src/app/shared/components/dmc-input/dmc-input.component.ts
@@ -1,25 +1,31 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, forwardRef, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { BaseControl } from '../base-control';
 
 @Component({
-  selector: 'dmc-input',
-  templateUrl: './dmc-input.component.html',
-  styleUrls: ['./dmc-input.component.scss']
+    selector: 'dmc-input',
+    templateUrl: './dmc-input.component.html',
+    styleUrls: ['./dmc-input.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => DmcInputComponent),
+            multi: true
+        }
+    ],
+    encapsulation: ViewEncapsulation.None
 })
-export class DmcInputComponent implements OnInit, OnChanges {
-  @Input() public name: string;
-  @Input() public placeholder: string;
-  @Input() public maxTextLength: string;
-  @Input() public type = 'text';
-  @Input() public ngModel: string;
+export class DmcInputComponent extends BaseControl<string> implements OnInit {
+    @Input() public placeholder: string;
+    @Input() public maxTextLength: string;
+    @Input() public type = 'text';
 
-  @Output() private readonly onChange = new EventEmitter();
+    constructor() {
+        super();
+    }
 
-  constructor() { }
-
-  public ngOnInit(): void { }
-
-  public ngOnChanges(changes: SimpleChanges): void {
-    this.onChange.emit(changes);
-  }
-
+    public ngOnInit(): void {
+        super.ngOnInit();
+    }
 }
